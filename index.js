b7b6f1aeed10f013c0d66a7a57bd1500132b235b
Saif Uddin Ahmed <neononezero@gmail.com>
2021-10-30 03:08:03 +0600

    Check api added

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/check', (req, res) => {
+    res.send('Check the server');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
