6a98552832350c2b1d1270fd9e49177bd5bddb4d
Saif Uddin Ahmed <neononezero@gmail.com>
2021-10-31 00:27:00 +0600

    Destinations and Attractions Places added in database

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 7000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xffah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -11,7 +16,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected');
+        const database = client.db('topDestination');
+        const destinationCollection = database.collection('destinations');
+        const attractivePlaceCollection = database.collection('attractivePlaces');
+
+        //GET Top Destinations API
+        app.get('/destinations', async (req, res) => {
+            const cursor = destinationCollection.find({});
+            const destinations = await cursor.toArray();
+
+            res.send(destinations);
+        })
+
+        //GET Top Attractions API
+        app.get('/attractive-place', async (req, res) => {
+            const cursor = attractivePlaceCollection.find({});
+            const attractivePlaces = await cursor.toArray();
+
+            res.send(attractivePlaces);
+        })
+
     }
     finally {
         // await client.close();
@@ -21,6 +45,10 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/services', (req, res) => {
+    res.send()
 })
 
 app.get('/check', (req, res) => {
